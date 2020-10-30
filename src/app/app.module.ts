5b2605372e579589dbce0126e6f5d9209392220f
rsurendra <surendra.rai@idexcel.com>
2020-10-31 03:12:16 +0530

    PWA Feature Added in demo poc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { TensorGoComponent } from './tensor-go/tensor-go.component';
 import { TensorGoService } from './tensor-go.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TensoregoInterceptor } from './tensorego-interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { TensoregoInterceptor } from './tensorego-interceptor';
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     TensorGoService,
--- src/app/tensorego-interceptor.ts
+++ src/app/tensorego-interceptor.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { TensorGoService } from './tensor-go.service';
+import { retry, catchError } from 'rxjs/operators';
 @Injectable()
 export class TensoregoInterceptor implements HttpInterceptor {
 
@@ -11,12 +12,25 @@ export class TensoregoInterceptor implements HttpInterceptor {
    
     let newHeaders = httpRequest.headers;
     const token = this.loginService.getTokenTensorGo();
-
     if(token){
       newHeaders= newHeaders.append('Authorization', token);
     }
     const authReq = httpRequest.clone({headers: newHeaders});
-    return next.handle(authReq);
-  }
 
+    return next.handle(authReq)
+    .pipe(
+      retry(1),
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage='';
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Errors:${error.error.message}`;
+        } else {
+          errorMessage = `Error Code : ${error.status} \n  Message: ${error.message}`;
+        }
+        alert(errorMessage);
+        return throwError(errorMessage)
+      })
+    )
+  }
+  
 }
